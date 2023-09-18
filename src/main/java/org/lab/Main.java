c6f330d1fd002614ce863d7d43a9315e8939172d
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-09-19 00:47:31 +0300

    Added showing all books feature

--- src/main/java/org/lab/Main.java
+++ src/main/java/org/lab/Main.java
@@ -1,6 +1,7 @@
 package org.lab;
 
 import javax.lang.model.element.Name;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +9,7 @@ public class Main {
     public static int getNumberInputInRange(Scanner in, int range) {
         while (in.hasNext()) {
             if (in.hasNextInt()) {
-                int val = in.nextInt();
+                int val = Integer.parseInt(in.nextLine());
                 if (val >= 1 && val < range) { // <-- from "0" to "range".
                     return val;
                 }
@@ -21,23 +22,23 @@ public class Main {
     }
     public static void displayBook(Book book) {
         if (book == null) {
-            System.out.println("No book was found with this title");
+            System.out.println("No book was found with this data");
             return;
         }
-        System.out.printf("Title: %s", book.title);
-        System.out.printf("Author: %s", book.author);
-        System.out.printf("ISBN: %s", book.ISBN);
-        System.out.printf("Release Year: %s", book.releaseYear);
+        System.out.printf("Title: %s\n", book.title);
+        System.out.printf("Author: %s\n", book.author);
+        System.out.printf("ISBN: %s\n", book.ISBN);
+        System.out.printf("Release Year: %s\n", book.releaseYear);
     }
     public static void main(String[] args) {
         Library library = new Library();
         Scanner scanner = new Scanner(System.in);
         Boolean isRunning = true;
         while(isRunning) {
-            System.out.print("Enter a number of action:\n1 - Add book\n2 - Search book by title\n3 - Remove book by ISBN\n4 - Show all books\n5 - Exit application");
+            System.out.print("Enter a number of action:\n1 - Add book\n2 - Search book by title\n3 - Remove book by ISBN\n4 - Show all books\n5 - Exit application\n");
             int action = getNumberInputInRange(scanner, 5);
             switch (action) {
-                case 1:
+                case 1 -> {
                     System.out.println("Enter title: ");
                     String title = scanner.nextLine();
                     System.out.println("Enter author: ");
@@ -47,15 +48,39 @@ public class Main {
                     System.out.println("Enter release year: ");
                     int releaseYear = getNumberInputInRange(scanner, 2023);
                     library.AddBook(new Book(title, author, ISBN, releaseYear));
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Enter title: ");
                     String bookTitle = scanner.nextLine();
                     displayBook(library.GetBookByTitle(bookTitle));
-                    break;
-                case 3:
-
+                }
+                case 3 -> {
+                    System.out.println("Enter ISBN: ");
+                    String bookISBN = scanner.nextLine();
+                    Book removedBook = library.RemoveBook(bookISBN);
+                    if (removedBook != null) System.out.println("Removed Book: ");
+                    displayBook(removedBook);
+                }
+                case 4 -> {
+                    ArrayList<Book> books = library.ShowBooks();
+                    if (books.isEmpty()) {
+                        System.out.println("No books yet");
+                        break;
+                    }
+                    for (Book book : books) {
+                        displayBook(book);
+                        System.out.println();
+                    }
+                }
+                case 5 -> {
+                    isRunning = false;
+                    System.exit(0);
+                }
             }
+            System.out.println("Continue? yes/no");
+            String answer = scanner.nextLine();
+            if(answer.toLowerCase() == "yes") break;
+
         }
 
     }
